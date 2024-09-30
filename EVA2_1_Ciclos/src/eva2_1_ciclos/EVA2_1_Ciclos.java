eec1b736ae6f8f6d217e88d14eb8e610560769c9
David8Omar32 <L24550248@chihuahua2.tecnm.mx>
2024-09-30 16:55:34 -0600

    Add files via upload

---
+++ EVA2_1_Ciclos/src/eva2_1_ciclos/EVA2_1_Ciclos.java
@@ -0,0 +1,11 @@
+package eva2_1_ciclos;
+public class EVA2_1_Ciclos {
+    public static void main(String[] args) {
+        for(int i=1; i<=100; i++){
+            System.out.println(i);
+            double a =i + 0.5;
+            System.out.println(a);
+        }
+    }
+    
+}
